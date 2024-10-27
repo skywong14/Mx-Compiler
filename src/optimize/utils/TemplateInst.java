24319e7e1451752d0070994951607779c3764fba
skywong14 <skywong0014@gmail.com>
2024-10-28 03:32:48 +0800

    optimize: callStmt

---
+++ src/optimize/utils/TemplateInst.java
@@ -0,0 +1,29 @@
+package optimize.utils;
+
+import ASM.inst.ASMInst;
+
+import java.util.ArrayList;
+
+public class TemplateInst extends ASMInst {
+    ArrayList<ASMInst> insts;
+
+    public TemplateInst(ArrayList<ASMInst> insts) {
+        this.insts = insts;
+    }
+
+    public void add(ASMInst inst) {
+        insts.add(inst);
+    }
+    public void add(ArrayList<ASMInst> insts) {
+        this.insts.addAll(insts);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (ASMInst inst : insts) {
+            sb.append(inst).append("\n");
+        }
+        return sb.toString();
+    }
+}
